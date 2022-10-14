dc93e4380c1bc1be5f04ffb2df8452635fb8ef9b
Ilya Korzhavin <ilya.korzhavin@innowise-group.com>
2022-10-14 12:15:40 +0300

    Add the IndexedDB project

--- indexed-db/scripts.js
+++ indexed-db/scripts.js
@@ -1,25 +1,87 @@
 let db;
 
-// Name and Version number
-let openRequest = indexedDB.open('test', 1);
+const DATABASE_VERSION = 2;
+const DATABASE_NAME = 'test';
+const USER_TABLE = 'user';
 
-// console.log(openRequest);
-
-openRequest.onerror = (event) => {
-  console.log('DB: Error');
-  db = event.target.result;
+const TransactionTypes = {
+  READ_ONLY: 'readonly',
+  READ_WRITE: 'readwrite',
 };
 
-openRequest.onsuccess = (event) => {
-  console.log('DB: Opened');
-  db = event.target.result;
-};
+function initDB() {
+  const openRequest = indexedDB.open(DATABASE_NAME, DATABASE_VERSION);
 
-openRequest.onupgradeneeded = (event) => {
-  console.log('DB: Upgrade Needed');
-  db = openRequest.result;
+  openRequest.onerror = (event) => {
+    console.log('DB: Error');
+    db = event.target.result;
+  };
 
-  if (!db.objectStoreNames.contains('user')) {
-    db.createObjectStore('user', { keyPath: 'id', autoIncrement: true });
-  }
-};
+  openRequest.onsuccess = (event) => {
+    console.log('DB: Opened');
+    db = event.target.result;
+  };
+
+  openRequest.onupgradeneeded = (event) => {
+    console.log('DB: Upgrade Needed');
+    db = openRequest.result;
+
+    if (!db.objectStoreNames.contains(USER_TABLE)) {
+      db.createObjectStore(USER_TABLE, { keyPath: 'id', autoIncrement: true });
+    }
+  };
+}
+
+// Change db version to emit onupgradeneeded event
+function alterDatabase() {}
+
+function addUser() {
+  const openRequest = indexedDB.open(DATABASE_NAME, DATABASE_VERSION);
+
+  openRequest.onsuccess = (event) => {
+    db = event.target.result;
+
+    const transaction = db.transaction(USER_TABLE, TransactionTypes.READ_WRITE);
+    const userTable = transaction.objectStore(USER_TABLE);
+
+    const user = {
+      name: 'Alex Rubis',
+      age: 26,
+    };
+
+    const request = userTable.add(user);
+
+    request.onsuccess = () => {
+      console.log('DB: User Added');
+    };
+
+    request.onerror = (event) => {
+      console.log('DB: Error User Adding');
+    };
+  };
+}
+
+function getUsers() {
+  const openRequest = indexedDB.open(DATABASE_NAME, DATABASE_VERSION);
+
+  openRequest.onsuccess = (event) => {
+    db = event.target.result;
+
+    const transaction = db.transaction(USER_TABLE);
+    const objectStore = transaction.objectStore(USER_TABLE);
+
+    objectStore.get(1).onsuccess = (event) => {
+      console.log('DB: Get by id 1');
+      console.log(event.target.result);
+    };
+
+    objectStore.getAll().onsuccess = (event) => {
+      console.log('DB: Get All');
+      console.log(event.target.result);
+    };
+  };
+}
+
+initDB();
+// addUser();
+getUsers();
